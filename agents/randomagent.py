42496677b462548124241d16f86d545a410bb6b8
Andreas Petrov <nailer@gmail.com>
2022-10-20 11:36:32 +0200

    Prevent random agent from randomly challenging draw 4s since it's probably the most disadvantageous move

--- agents/randomagent.py
+++ agents/randomagent.py
@@ -7,7 +7,10 @@ class RandomAgent(UnoAgent):
     def get_action(self, observations, **kwargs) -> Action:
         action_space = observations[-1].action_space()
         play_card_actions = [action for action in action_space if isinstance(action, PlayCard)]
+        accept_actions = [action for action in action_space if isinstance(action, AcceptDrawFour)]
         if play_card_actions:
             return random.sample(play_card_actions, 1)[0]
+        if accept_actions:
+            return accept_actions[0]
         else:
             return random.sample(action_space, 1)[0]
